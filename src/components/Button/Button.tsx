9f5e5e62bc96c3dcce732c8a3959c391aaf36620
thiagomeneguzzi <thiagonmeneguzzi@gmail.com>
2023-05-29 21:24:13 -0300

    try new config

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-import styles from './Button.scss'
+import styles from './Button.scss';
 
 export type ButtonProps = {
     label: string;
-}
+};
 
 const Button = (props: ButtonProps) => {
     return <button className={styles.Button}>{props.label}</button>;
